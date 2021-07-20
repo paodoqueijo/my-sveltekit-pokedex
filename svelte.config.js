90ca331bd0f50e0204c78e1ac9e61c41d58c40ae
michael_sattler <sattlermichi@gmail.com>
2021-07-20 22:50:26 +0200

    chore: add build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import path from 'path';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +11,12 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+		}),
 		vite: {
 			resolve: {
 				extensions: ['.ts', '.tsx', '.js'],
